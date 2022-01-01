9217db922442e5bbaea274ae2ff38170c8e11307
Salemkd <85119912+Salemah@users.noreply.github.com>
2022-01-01 23:58:38 +0600

    doctor appointmne form updated

--- src/Pages/Home/Doctorappointment/Doctorapoinemnt.js
+++ src/Pages/Home/Doctorappointment/Doctorapoinemnt.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import UseAuth from '../../../Hooks/UseAuth';
+import { useForm } from 'react-hook-form';
 
 const Doctorapoinemnt = () => {
-    const {did} = useParams();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { user } = UseAuth();
+    const onSubmit = data => {
+        console.log(data)
+    };
+    const { did } = useParams();
     return (
         <div>
-            <h1>This is doctor: {did} </h1>
+            <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
+
+                <input defaultValue={user.displayName} {...register("name")} /> <br/>
+
+                <input defaultValue={user.email} {...register("email", { required: true })} />
+                {errors.email && <span className="error">This field is required</span>}
+                
+               
+
+                <input type="submit" />
+            </form>
         </div>
     );
 };
