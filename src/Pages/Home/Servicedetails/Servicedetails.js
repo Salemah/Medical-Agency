d0fff966b94fc7634ea8de1deac7a38bd88134c0
Salemkd <85119912+Salemah@users.noreply.github.com>
2022-01-04 22:15:26 +0600

    service details updated

--- src/Pages/Home/Servicedetails/Servicedetails.js
+++ src/Pages/Home/Servicedetails/Servicedetails.js
@@ -4,43 +4,40 @@ import { useParams } from 'react-router-dom';
 import './Servicedetails.css';
 
 const Servicedetails = () => {
-    const [details,setDetails] = useState({});
-    const {id} = useParams();
-    const url =`/services.json`;
-    useEffect(()=>{
+    const [details, setDetails] = useState({});
+    const { id } = useParams();
+    const url = `/services.json`;
+    useEffect(() => {
         fetch(url)
-        .then(res =>res.json())
-        .then(data => {
-            const exist = data.find(pd=>pd.id == id );
-            setDetails(exist);
-        })
-    },[]);
+            .then(res => res.json())
+            .then(data => {
+                const exist = data.find(pd => pd.id == id);
+                setDetails(exist);
+            })
+    }, []);
     return (
         <div className="single-service">
-            <div class="service-details">
-                <div class="left-details">
-                     <h4>OUR</h4>
-                    <h1>Specialized Team</h1>
-                    <hr/>
-                    <p><q>We are a team of Doctor, hygienists and receptionists who work together to ensure that you receive the best treatment that you require at a very time that suits you.</q></p>
-                    <hr/> 
-             
-                </div>
-                <div class="left-details">
-                <Container fluid="md">
-  <Row>
-      <img src={details.img} alt=""/>
-    <h3>Name: {details.name}</h3>
-                    <h2>Service Id : {id}</h2>
-  </Row>
-</Container>
-                    
-                </div>
-
-            </div>
-           
             
-        </div>
+                <Container>
+                    <Row>
+                        <Col class="left-details"><h4>OUR</h4>
+                            <h1>Specialized Team</h1>
+                            <hr />
+                            <p className="pessage"><q>We are a team of Doctor, hygienists and receptionists who work together to ensure that you receive the best treatment that you require at a very time that suits you.</q></p>
+                            <hr /> </Col>
+                        <Col class="left-details">
+                            <img  src={details.img} alt=""/>
+                            <h2>{details.name}</h2>
+                           </Col>
+                        <Col class="left-details">
+                            <p  className="pessage">{details.details}</p>
+                            </Col>
+
+                    </Row>
+
+                </Container>
+                </div>
+               
     );
 };
 
